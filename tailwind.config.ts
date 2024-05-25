f4a1dba76415f13d333f37552ca2b433db2f3dff
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-05-25 08:23:12 -0600

    🚧 tailwind config.
    - update tailwind base config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#ffffff',
+        accent: '#1f2937', // Dark gray for accent
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -17,4 +22,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;

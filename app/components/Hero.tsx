3c5552c1ab90fddab9741025803638db8e441abb
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-05-26 00:26:56 -0600

    🚧 hero:
    - update hero slider for a better ui

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { Genre, Movie } from '../types/movie';
 import StarRating from './StarRating';
+import Link from 'next/link';
+import './Hero.css';
 
 interface HeroProps {
   movies: Movie[];
@@ -18,6 +20,10 @@ const Hero: React.FC<HeroProps> = ({ movies }) => {
     );
   };
 
+  const scrollToSlide = (index: number) => {
+    setCurrent(index);
+  };
+
   useEffect(() => {
     const timeoutRef = setTimeout(nextSlide, 5000);
     return () => {
@@ -29,75 +35,69 @@ const Hero: React.FC<HeroProps> = ({ movies }) => {
   if (!movie) return null;
 
   return (
-    <div className="relative w-full h-[70vh] md:h-[60vh] lg:h-[70vh] overflow-hidden mb-8">
-      {movies.map((movie, index) => (
-        <div
-          key={movie.id}
-          className={`absolute inset-0 transition-opacity duration-1000 ${index === current ? 'opacity-100' : 'opacity-0'}`}
-        >
-          <Image
-            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-            alt={movie.title}
-            layout="fill"
-            objectFit="cover"
-            className="object-cover"
-          />
-          <div className="absolute inset-0 bg-black bg-opacity-50 flex items-start justify-start p-4">
-            <div className="text-left text-white max-w-full md:max-w-2xl mt-8 md:mt-16 ml-4 md:ml-8">
-              <h1 className="text-2xl md:text-4xl font-bold mb-2 transition-opacity duration-300">
-                {movie.title}
-              </h1>
-              <div className="flex flex-wrap items-center mb-2">
-                <StarRating rating={movie.vote_average} />
-                <span className="ml-2 text-sm md:text-lg">
-                  {movie.vote_average}
-                </span>
-                <span className="ml-2 text-sm md:text-lg">
-                  ({movie.vote_count} Reviews)
-                </span>
-                <span className="ml-2 text-sm md:text-lg">
-                  • {new Date(movie.release_date).getFullYear()}
-                </span>
-                <span className="ml-2 text-sm md:text-lg">
-                  • {Math.floor(movie.runtime / 60)}h {movie.runtime % 60}min
-                </span>
+    <div className="hero-container relative w-full h-[70vh] md:h-[60vh] lg:h-[70vh] overflow-hidden mb-8">
+      <div
+        className="hero-content flex transition-transform duration-500"
+        style={{ transform: `translateX(-${current * 100}%)` }}
+      >
+        {movies.map((movie, index) => (
+          <div key={movie.id} className="hero-item relative w-full h-full">
+            <Link href={`/movie/${movie.id}`}>
+              <div className="hero--image-container">
+                <Image
+                  src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                  alt={movie.title}
+                  className="hero-image"
+                  layout="fill"
+                  objectFit="cover"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-start justify-start p-4">
+                  <div className="text-left text-white max-w-full md:max-w-2xl mt-8 md:mt-16 ml-4 md:ml-8">
+                    <h1 className="text-2xl md:text-4xl font-bold mb-2 transition-opacity duration-300">
+                      {movie.title}
+                    </h1>
+                    <div className="flex flex-wrap items-center mb-2">
+                      <StarRating rating={movie.vote_average} />
+                      <span className="ml-2 text-sm md:text-lg">
+                        {movie.vote_average}
+                      </span>
+                      <span className="ml-2 text-sm md:text-lg">
+                        ({movie.vote_count} Reviews)
+                      </span>
+                      <span className="ml-2 text-sm md:text-lg">
+                        • {new Date(movie.release_date).getFullYear()}
+                      </span>
+                      <span className="ml-2 text-sm md:text-lg">
+                        • {Math.floor(movie.runtime / 60)}h {movie.runtime % 60}
+                        min
+                      </span>
+                    </div>
+                    <div className="flex flex-wrap items-center mb-2 space-x-2">
+                      {movie.genres.map((genre: Genre) => (
+                        <span
+                          key={genre.id}
+                          className="bg-gray-800 text-white py-1 px-2 rounded text-xs md:text-sm"
+                        >
+                          {genre.name}
+                        </span>
+                      ))}
+                    </div>
+                    <p className="text-sm md:text-lg">{movie.overview}</p>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-wrap items-center mb-2 space-x-2">
-                {movie.genres.map((genre: Genre) => (
-                  <span
-                    key={genre.id}
-                    className="bg-gray-800 text-white py-1 px-2 rounded text-xs md:text-sm"
-                  >
-                    {genre.name}
-                  </span>
-                ))}
-              </div>
-              <p className="text-sm md:text-lg">{movie.overview}</p>
-            </div>
+            </Link>
           </div>
-        </div>
-      ))}
-      <div className="absolute inset-0 flex items-center justify-between px-4 opacity-0 hover:opacity-100 transition-opacity duration-300">
-        <button
-          onClick={() =>
-            setCurrent((prevCurrent) =>
-              prevCurrent === 0 ? movies.length - 1 : prevCurrent - 1
-            )
-          }
-          className="p-2 md:p-4 bg-gray-800 bg-opacity-50 text-white rounded-full focus:outline-none hover:bg-gray-700"
-        >
-          &#8249;
-        </button>
-        <button
-          onClick={() =>
-            setCurrent((prevCurrent) =>
-              prevCurrent === movies.length - 1 ? 0 : prevCurrent + 1
-            )
-          }
-          className="p-2 md:p-4 bg-gray-800 bg-opacity-50 text-white rounded-full focus:outline-none hover:bg-gray-700"
-        >
-          &#8250;
-        </button>
+        ))}
+      </div>
+      <div className="carousel-navigation absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-2 z-10">
+        {movies.map((_, index) => (
+          <button
+            key={index}
+            className={`carousel-dot ${index === current ? 'active' : ''}`}
+            onClick={() => scrollToSlide(index)}
+          ></button>
+        ))}
       </div>
     </div>
   );

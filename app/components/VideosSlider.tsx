72fa8ebff0c7fa94ddacde0bea4f0e7d5c09d567
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-05-26 00:26:40 -0600

    🚧 video slider:
    - add video slider component

--- app/components/VideosSlider.tsx
+++ app/components/VideosSlider.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { Video } from '../types/movie';
 import './VideoSlider.css';
@@ -7,48 +9,45 @@ interface VideosSliderProps {
 }
 
 const VideosSlider: React.FC<VideosSliderProps> = ({ videos }) => {
+  const scrollToVideo = (index: number) => {
+    const container = document.querySelector('.carousel-content');
+    if (container) {
+      const scrollAmount = container.clientWidth * index;
+      container.scrollTo({
+        left: scrollAmount,
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <div className="my-8">
       <h3 className="text-xl font-bold mb-4">Trailers</h3>
-      <section className="carousel" aria-label="Gallery">
-        <ol className="carousel__viewport">
+      <div className="carousel-container">
+        <div className="carousel-content">
           {videos.map((video, index) => (
-            <li
-              key={video.id}
-              id={`carousel__slide${index + 1}`}
-              className="carousel__slide"
-            >
-              <div className="carousel__snapper">
-                <iframe
-                  className="absolute top-0 left-0 w-full h-full"
-                  src={`https://www.youtube.com/embed/${video.key}`}
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                  title={video.name}
-                ></iframe>
-                <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-75 text-white p-2 text-sm">
-                  <span>{video.name}</span>
-                </div>
-              </div>
-            </li>
+            <div key={video.id} className="carousel-item">
+              <iframe
+                className="video-frame"
+                src={`https://www.youtube.com/embed/${video.key}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title={video.name}
+              ></iframe>
+            </div>
           ))}
-        </ol>
-        <aside className="carousel__navigation">
-          <ol className="carousel__navigation-list">
-            {videos.map((_, index) => (
-              <li key={index} className="carousel__navigation-item">
-                <a
-                  href={`#carousel__slide${index + 1}`}
-                  className="carousel__navigation-button"
-                >
-                  Go to slide {index + 1}
-                </a>
-              </li>
-            ))}
-          </ol>
-        </aside>
-      </section>
+        </div>
+        <div className="carousel-navigation">
+          {videos.map((_, index) => (
+            <button
+              key={index}
+              className="carousel-dot"
+              onClick={() => scrollToVideo(index)}
+            ></button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
